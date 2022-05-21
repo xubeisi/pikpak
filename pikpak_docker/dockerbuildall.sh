7c064a33479e5342aa694f4f0bd8b774c11e92ec
Xu, Beisi <Beisi.Xu@STJUDE.ORG>
2022-05-21 12:51:16 -0500

    build

--- pikpak_docker/dockerbuildall.sh
+++ pikpak_docker/dockerbuildall.sh
@@ -2,3 +2,4 @@ echo linux/386,linux/amd64,linux/arm32v6,linux/arm32v7,linux/arm64v8 | tr "," "\
 do
     docker buildx build . --platform $l --output "type=image,push=true" --file Dockerfile --tag xubeisi/pikpak:${l//\//_} &
 done
+docker system prune -a
